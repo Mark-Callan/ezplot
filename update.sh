6211cd2529004a1083eb13608f1ef9ed78f2bb05
Mark Callan <mark.l.callan@gmail.com>
2021-06-11 14:57:30 -0700

    Updates

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-hosts="10.0.0.31 10.0.0.41 10.0.0.51"
+#hosts="10.0.0.51 10.0.0.31 10.0.0.41"
+hosts="10.0.0.51"
 
 for h in $hosts; do
 
-  echo $h
-  ssh -A -X $h "cd source/ezplot ; git pull ; make install ;"
+  ssh $h "cd source/ezplot ; git pull; make install ;" 
 
 done
 
